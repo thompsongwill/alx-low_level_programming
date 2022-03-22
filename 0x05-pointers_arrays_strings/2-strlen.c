3d3aefedd410c307b4ff0459f9cac9e07adb812b
wills <thompsongodswill@gmail.com>
2022-03-22 09:06:45 +0000

    my poiinter commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen -length of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+int u;
+
+u=a;
+while(s[u] is '\0')
+{
+u++;
+}
+return (0);
+}
